4b7511a4d976a3f1358df7cf3cbf1ed0c036c555
logan-fink <logan.fink@state.co.us>
2018-09-09 02:09:20 +0000

    Updated run script to v2.0

--- run_type_pipe_2.0.sh
+++ run_type_pipe_2.0.sh
@@ -3,5 +3,5 @@ if find logfile.txt;
 then
     cat logfile.txt >> logfile_prev.txt
 fi
-/home/staphb/scripts/type_pipe_1.6.sh |& tee logfile.txt
+/home/staphb/scripts/type_pipe_2.0.sh |& tee logfile.txt
 
