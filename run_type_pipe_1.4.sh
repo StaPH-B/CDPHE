8a28c3723bf398809773e46b321d3bb69d1671fc
Curtis Kapsak <kapsakcj@gmail.com>
2018-09-09 01:39:02 +0000

    Updated run script to v1.4

--- run_type_pipe_1.4.sh
+++ run_type_pipe_1.4.sh
@@ -3,4 +3,4 @@ if find logfile.txt;
 then
     cat logfile.txt >> logfile_prev.txt
 fi
-/home/staphb/scripts/type_pipe_1.3.sh |& tee logfile.txt
+/home/staphb/scripts/type_pipe_1.4.sh |& tee logfile.txt
