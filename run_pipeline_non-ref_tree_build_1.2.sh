d739347dff36a8fd18bcab17e2b5284e464495c0
logan-fink <logan.fink@state.co.us>
2018-10-17 22:30:23 +0000

    Added time stamp to end of entire run-time of pipeline_tree_build

--- run_pipeline_non-ref_tree_build_1.2.sh
+++ run_pipeline_non-ref_tree_build_1.2.sh
@@ -3,4 +3,4 @@ if find logfile_non-ref.txt;
 then
     cat logfile_non-ref.txt >> logfile_non-ref_prev.txt
 fi
-/home/staphb/scripts/pipeline_non-ref_tree_build_1.2.sh |& tee logfile_non-ref.txt
+command time -v /home/staphb/scripts/pipeline_non-ref_tree_build_1.2.sh |& tee logfile_non-ref.txt
