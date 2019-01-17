4eee4522d23c3c746fa267edc55182bac8736431
logan-fink <logan.fink@state.co.us>
2019-01-17 21:42:23 +0000

    Added in print docker commands statements for the logfile

--- pipeline_non-ref_tree_build_1.4-dockerized.sh
+++ pipeline_non-ref_tree_build_1.4-dockerized.sh
@@ -3,8 +3,52 @@
 #Usage: script to create a reference free phylogenetic tree from a set of fastq files
 #Permission to copy and modify is granted without warranty of any kind
 
+version="2.3"
+#Print out the line after the current line in the script, and print the evaluation
+#of how it will be executed
+print_next_command() {
+    current_line=$(($1+1))
+    range=$(($1+1))
+    x=0
+    while [ $x == 0 ]; do
+        p=$(sed -n ${range}'p' /home/staphb/scripts/type_pipe_$version-dockerized.sh)
+        if [[ $p == *\\ ]]; then
+            range=$(($range+1))
+        else
+            x+=1
+        fi
+    done
+    if [[ $range == $current_line ]]; then
+        #echo $(sed -n ${current_line}'p' /home/staphb/scripts/type_pipe_$version-dockerized.sh)
+        line_data=$(sed -n ${current_line}'p' /home/staphb/scripts/type_pipe_$version-dockerized.sh)
+        line_data=$(echo $line_data | sed "s/'//g")
+        #echo line_data
+        output_prefix=''
+        if [[ $line_data == *'>'* ]]; then
+            end=${line_data##*>}
+            output_prefix='Output file: '
+            line_data=${line_data%%>*}
+        fi
+        eval echo $line_data
+        eval echo $output_prefix$end
+    else
+        #echo $(sed -n ${current_line}','${range}'p' /home/staphb/scripts/type_pipe_$version-dockerized.sh)
+        line_data=$(sed -n ${current_line}','${range}'p' /home/staphb/scripts/type_pipe_$version-dockerized.sh)
+        line_data=$(echo $line_data | sed "s/'//g")
+        #echo $line_data
+        output_prefix=''
+        if [[ $line_data == *'>'* ]]; then
+            end=${line_data##*>}
+            output_prefix='Output file: '
+            line_data=${line_data%%>*}
+        fi
+        eval echo $line_data
+        eval echo $output_prefix$end
+    fi
+}
+
 #This function will check if the file exists before trying to remove it
-remove_file () {
+remove_file() {
     if [ -e $1 ];then
         echo "File $1 has been removed"
         rm -rf $1
@@ -12,7 +56,7 @@ remove_file () {
 }
 
 #This function will check to make sure the directory doesn't already exist before trying to create it
-make_directory () {
+make_directory() {
     if [ -e $1 ]; then
         echo "Directory "$1" already exists"
     else
@@ -75,24 +119,25 @@ id=("${id[@]}" "${srr[@]}") #Combine the automatically generated list with the S
 remove_file tmp
 
 ##### Fetch and fastq-dump all reads from NCBI identified by "SRR" #####
-for i in ${id[@]}; do
-    if [[ $i =~ "SRR" ]]; then
-        if (find ./*$i*); then
-            echo "Files are here."
-        else
-            echo 'prefetching reads '$i' using sratoolkit docker container...'
-            export i
-            docker run -e i --rm=True -u $(id -u):$(id -g) -v $PWD:/data staphb/sratoolkit:2.9.2 /bin/bash -c \
-            'prefetch -O /data ${i}'
-            echo 'now running fasterq-dump in container'
-            docker run -e i -e THREADS --rm=True -u $(id -u):$(id -g) -v $PWD:/data staphb/sratoolkit:2.9.2 /bin/bash -c \
-            'fasterq-dump --skip-technical --split-files -t /data/tmp-dir -e ${THREADS} -O /data -p ${i}.sra'
-            mv ${i}.sra_1.fastq ${i}_1.fastq
-            mv ${i}.sra_2.fastq ${i}_2.fastq
-            pigz ${i}_1.fastq
-            pigz ${i}_2.fastq
-            rm ${i}.sra
-        fi
+for i in ${srr[@]}; do
+    echo $i
+    if [[ -n "$(find *$i* 2>/dev/null)" ]]; then
+        echo "Files are here."
+    else
+        export i
+        echo 'prefetching '$i'...'
+        print_next_command $LINENO ${i}
+        docker run -e i --rm=True -u $(id -u):$(id -g) -v $PWD:/data staphb/sratoolkit:2.9.2 /bin/bash -c \
+        'prefetch -O /data '${i}
+        echo 'now running fasterq-dump in container'
+        print_next_command $LINENO ${i}
+        docker run -e THREADS -e i --rm=True -u $(id -u):$(id -g) -v $PWD:/data staphb/sratoolkit:2.9.2 /bin/bash -c \
+        'fasterq-dump --skip-technical --split-files -t /data/tmp-dir -e ${THREADS} -p '${i}'.sra'
+        mv ${i}.sra_1.fastq ${i}_1.fastq
+        mv ${i}.sra_2.fastq ${i}_2.fastq
+        pigz ${i}_1.fastq
+        pigz ${i}_2.fastq
+        rm ${i}.sra
     fi
 done
 remove_file tmp-dir
@@ -106,10 +151,12 @@ for i in *R1_001.fastq.gz; do
         continue
     else
         echo "LYVESET CONTAINER RUNNING SHUFFLEREADS.PL"
+        print_next_command $LINENO ${i}
         docker run --rm=True -v $PWD:/data -u $(id -u):$(id -g) staphb/lyveset:2.0.1 \
         run_assembly_shuffleReads.pl /data/${b}"_R1_001.fastq.gz" /data/${b}"_R2_001.fastq.gz" > clean/${b}.fastq;
         echo ${b};
         echo "LYVESET CONTAINER RUNNING TRIMCLEAN.PL"
+        print_next_command $LINENO ${i}
         docker run --rm=True -v $PWD:/data -u $(id -u):$(id -g) staphb/lyveset:2.0.1 \
         run_assembly_trimClean.pl -i /data/clean/${b}.fastq -o /data/clean/${b}.cleaned.fastq.gz --nosingletons --numcpus ${THREADS};
         remove_file clean/${b}.fastq;
@@ -121,9 +168,11 @@ for i in *_1.fastq.gz; do
         continue
     else
         echo '(run_assembly_shuffleReads.pl)Interleaving reads for:'${c}' using lyveset docker container'
+        print_next_command $LINENO ${i}
         docker run --rm=True -v $PWD:/data -u $(id -u):$(id -g) staphb/lyveset:2.0.1 \
         run_assembly_shuffleReads.pl /data/${b}"_1.fastq.gz" /data/${b}"_2.fastq.gz" > clean/${b}.fastq;
         echo '(run_assembly_trimClean.pl) Trimming/cleaning reads for:'${c}' using lyveset docker container'
+        print_next_command $LINENO ${i}
         docker run --rm=True -v $PWD:/data -u $(id -u):$(id -g) staphb/lyveset:2.0.1 \
         run_assembly_trimClean.pl -i /data/clean/${b}.fastq -o /data/clean/${b}.cleaned.fastq.gz --nosingletons --numcpus ${THREADS};
         remove_file clean/${b}.fastq;
@@ -141,6 +190,7 @@ for i in ${id[@]}; do
         # make sure variable i is available to global env, to pass into container
         export i
         echo 'constructing assemblies for '$i', could take some time...'
+        print_next_command $LINENO ${i}
         docker run -e i -e THREADS --rm=True -v $PWD:/data -u $(id -u):$(id -g) staphb/spades:3.12.0 /bin/bash -c \
         'spades.py --pe1-12 /data/clean/${i}*.cleaned.fastq.gz --careful -t ${THREADS} -o /data/spades_assembly_trim/${i}/'
         rm -rf ./spades_assembly_trim/$i/corrected \
@@ -168,6 +218,7 @@ for i in ${id[@]}; do
         echo $spades_file
         export i
         echo 'constructing assemblies for '$i', second try...'
+        print_next_command $LINENO ${i}
         docker run -e i -e THREADS --rm=True -v $PWD:/data -u $(id -u):$(id -g) staphb/spades:3.12.0 /bin/bash -c \
         'spades.py -o /data/new_temp/spades_assemblies/${i}/ -t ${THREADS} --pe1-12 /data/new_temp/${i}_*.cleaned.fastq.gz --careful'
         rm -rf ./new_tmp/spades_assemblies/$i/corrected \
@@ -192,6 +243,7 @@ for i in ${id[@]}; do
     if [[ -e quast/${i}_output_file ]]; then
         echo "Skipping ${i}. It's SPAdes assembly has already been QUASTed."
     else
+        print_next_command $LINENO ${i}
         docker run --rm=True -v $PWD:/data -u $(id -u):$(id -g) staphb/quast:5.0.0 \
         quast.py /data/spades_assembly_trim/$i/contigs.fasta -t ${THREADS} -o /data/quast/$i
     fi
@@ -205,6 +257,7 @@ for i in ${id[@]}; do
     else
         export i
         echo "Prokka will now be run on "$i
+        print_next_command $LINENO ${i}
         docker run -e i -e THREADS --rm=True -u $(id -u):$(id -g) -v $PWD:/data staphb/prokka:1.13 /bin/bash -c \
         'prokka /data/spades_assembly_trim/$i/contigs.fasta --outdir /data/prokka/$i --prefix $i --cpus ${THREADS}'
     fi
@@ -216,10 +269,12 @@ cp prokka/*/*.gff gff_files/ #copy over the .gff files from prokka
 
 ##### Run roary using the .gff file folder #####
 rm -rf roary
+print_next_command $LINENO ${i}
 docker run -e i -e THREADS --rm=True -u $(id -u):$(id -g) -v $PWD:/data staphb/roary:3.12.0 /bin/bash -c \
 'roary -p ${THREADS} -e -n -v -f /data/roary /data/gff_files/*.gff'
 
 ##### Run raxml on the roary alignment to generate a tree #####
+print_next_command $LINENO ${i}
 docker run -e i --rm=True -u $(id -u):$(id -g) -v $PWD:/data staphb/lyveset:2.0.1 /bin/bash -c \
 'raxmlHPC -m GTRGAMMA -p 12345 -x 12345 -s /data/roary/core_gene_alignment.aln -# 100 -n phylo_output -f a'
 rm -rf raxml/
